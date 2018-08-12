8ecd5d13866f3a3bc26e79434ec210aeef3d8ea3
Masood Karimi <masoodkarimi@masoods-mbp.fios-router.home>
2018-08-12 10:00:20 -0400

    Initial Commit

---
+++ y86/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  y86
+//
+//  Created by Masood Karimi on 8/12/18.
+//  Copyright © 2018 Masood Karimi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
